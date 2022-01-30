6ae31a3e50b56cb980970e9aa142fb49865c6c85
kehindetomiwa <kehinde.tomiwa4@gmail.com>
2022-01-30 13:19:34 +0100

    first commit

---
+++ WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Docker_HTTPS_ASPNET_CORE
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
